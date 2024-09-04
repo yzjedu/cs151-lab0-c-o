2ce9d3a0b92acbd4cad1aa52b0bb629f0aa16ab1
cjcombariza <cjcombariza@loyola.edu>
2024-09-04 16:14:41 -0400

    Cameron, i finished my part

---
+++ .github/lab0.py
@@ -0,0 +1,22 @@
+# Programmers:  Cameron, Oreoluwa
+  # Course:  CS151, Dr. Isaacman
+  # Due Date: 9/12/24
+  # Lab Assignment: 0
+  # Problem Statement:  Output information about the programmers
+  # Data In: None, there is no user input in this program
+  # Data Out:  The names of the programmers and five statements about each of them
+  # Credits: [Is your code based on an example in the book, in class, or something else?
+print("")
+print ("my partner's name is Oreoluwa \nfive facts about them are: ")
+print ("1. she doesn't like dogs")
+print ("2. has 2 brothers")
+print ("3. she has a scar on her leg")
+print ("4. is a transfer student")
+print ("5. 9/4/24 is her first day at loyola (classes)\n")
+
+print ("my partner's name is Cameron")
+print ("he's from vegas")
+print ("lives in Lutherville,maryland")
+print ("has a dog")
+print ("is a com sci major")
+print ("played an rl1 elden ring run")
