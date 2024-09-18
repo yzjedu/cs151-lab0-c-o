93eafcdacf479dacf90b8930648901182f6c3b5a
Ms_Gigi <ogadebusoye@loyola.edu>
2024-09-17 22:43:41 -0400

    Final submission

--- .github/lab0.py
+++ .github/lab0.py
@@ -1,4 +1,4 @@
-# Programmers:  Cameron, Oreoluwa
+  # Programmers:  Cameron, Oreoluwa
   # Course:  CS151, Dr. Isaacman
   # Due Date: 9/12/24
   # Lab Assignment: 0
@@ -10,13 +10,13 @@ print("")
 print ("my partner's name is Oreoluwa \nfive facts about them are: ")
 print ("1. she doesn't like dogs")
 print ("2. has 2 brothers")
-print ("3. she has a scar on her leg")
+print ("3. she speaks two languages")
 print ("4. is a transfer student")
 print ("5. 9/4/24 is her first day at loyola (classes)\n")
 
-print ("my partner's name is Cameron")
-print ("he's from vegas")
-print ("lives in Lutherville, Maryland")
-print ("has a dog")
-print ("is a com sci major")
-print ("played an rl1 elden ring run")
+print ("1. my partner's name is Cameron")
+print ("2. he's from vegas")
+print ("3. he lives in Lutherville, Maryland")
+print ("4. he has a dog")
+print ("5. he is a com sci major")
+print ("6. he played an rl1 elden ring run")
